285991d8fc5caf153aa4cc55b59b02363c7fc230
kangmeoungoun <kes98202@naver.com>
2021-01-02 20:41:46 +0900

    회원 웹 기능 - 등록

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-![image](https://user-images.githubusercontent.com/40969203/103456119-93e89c80-4d36-11eb-85ba-042dafd4dd30.png)
-![image](https://user-images.githubusercontent.com/40969203/103456122-99de7d80-4d36-11eb-8fba-9be28b6c2fda.png)
+![image](https://user-images.githubusercontent.com/40969203/103456586-d6ac7380-4d3a-11eb-9482-77b4c91d75b7.png)
+![image](https://user-images.githubusercontent.com/40969203/103456587-db712780-4d3a-11eb-9198-26ee0416f481.png)
+![image](https://user-images.githubusercontent.com/40969203/103456589-df04ae80-4d3a-11eb-8dd4-2d8c2ef4406d.png)
+![image](https://user-images.githubusercontent.com/40969203/103456591-e1ff9f00-4d3a-11eb-9121-783e7785d867.png)
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,25 +1,33 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController{
-    //@Autowired
     private MemberService memberService;
 
-    //조립시점에만 완성하고 그 이후에 는 못바꿔야되는데 중간에도 바꿀수 있게 노출된다.
-    //생성하는 시점에만 하고 그이후에느 변경 못하도록.
-    //권장하는 스타일은 생성자를 통한 주입
     @Autowired
     public void setMemberService(MemberService memberService){
         this.memberService = memberService;
     }
 
-    //    @Autowired
-//    public MemberController(MemberService memberService){
-//        this.memberService = memberService;
-//    }
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
 
+        System.out.println("member : "+member.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
