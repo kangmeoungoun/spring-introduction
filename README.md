ea31e3865cdcb438a1dfad3b150f27bfac0d0084
kangmeoungoun <kes98202@naver.com>
2021-01-04 17:34:07 +0900

    AOP 가 필요한 상황

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-![image](https://user-images.githubusercontent.com/40969203/103509435-7c302600-4ea6-11eb-8302-9e27b7e38791.png)
-![image](https://user-images.githubusercontent.com/40969203/103509448-83efca80-4ea6-11eb-8bf2-97a58e891263.png)
-![image](https://user-images.githubusercontent.com/40969203/103509452-87835180-4ea6-11eb-84e0-bba47007431f.png)
+![image](https://user-images.githubusercontent.com/40969203/103515943-fcf51f00-4eb2-11eb-97d6-abf37b945d86.png)
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,10 +24,18 @@ public class MemberService{
      * */
     public Long join(Member member){
 
-        //중복 회원 검증
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            //중복 회원 검증
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish-start;
+            System.out.println("join = "+timeMs +"ms");
+        }
+
     }
 
     private void validateDuplicateMember(Member member){
